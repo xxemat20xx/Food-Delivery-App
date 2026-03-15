c8ab82d601a71db8782ea7a6fa55700bf8802465
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-03-16 01:00:04 +0800

    init commit

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,26 +1,64 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import validator from "validator";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String
+      type: String,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "Invalid email address"],
     },
     password: {
-        type: String
+      type: String,
     },
     role: {
-        type: String,
-        enum: ["customer", "staff", "admin"],
-        default: "customer"
+      type: String,
+      enum: ["customer", "staff", "admin"],
+      default: "customer",
     },
-    location:{
-        lat: Number,
-        lng: Number,
-    }
-}, {timestamp: true});
+    location: {
+      lat: Number,
+      lng: Number,
+    },
+    googleId: {
+      type: String,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otpHash: {
+      type: String,
+    },
+
+    otpExpires: {
+      type: Date,
+    },
+
+    otpAttempts: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+// pre-save password
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
