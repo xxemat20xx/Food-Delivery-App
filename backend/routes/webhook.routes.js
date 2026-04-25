2af1b1ee3b87a8479a26f6ccffbbcb502f1e3199
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-04-26 01:44:42 +0800

    commit fix for payment stat

--- backend/routes/webhook.routes.js
+++ backend/routes/webhook.routes.js
@@ -2,9 +2,16 @@ import express from "express";
 import { handleWebhook } from "../controller/webhook.controller.js";
 
 const router = express.Router();
+
+// Force capture of raw body for signature verification
 router.post(
   "/payment",
-  express.raw({ type: "application/json" }),
+  express.raw({
+    type: "application/json",
+    verify: (req, res, buf) => {
+      req.rawBody = buf; // explicitly store the buffer
+    },
+  }),
   handleWebhook,
 );
 
